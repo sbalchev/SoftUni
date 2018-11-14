f0062deea66de4009f49c40824dab4144ef5a308
Balchev <balchevstoycho@gmail.com>
2018-11-14 11:38:39 +0200

    Add files via upload

---
+++ InvetoryHeroicInventory.js
@@ -0,0 +1,19 @@
+
+function solve(input){
+    let heroData = [];
+
+    for (let i = 0; i < input.length; i++) {
+        let [heroName, heroLevel, items] = input[i].split(" / ");
+
+        let heroItems = [];
+
+        if(items != undefined){
+            heroItems = items.split(", ");
+        }
+
+        heroData.push({name: heroName, level: Number(heroLevel), items: heroItems});
+    }
+
+    let stringify = JSON.stringify(heroData);
+    console.log(stringify);
+}
